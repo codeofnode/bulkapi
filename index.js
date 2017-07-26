6014d35dee8b2f636e0273cfd0e74b35d7afce50
Ramesh Kumar <codeofnode@gmail.com>
2017-07-26 22:53:56 +0530

    readable code

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.MockResMethods = exports.default = undefined;
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; /* global global, Promise */
-
-
-var _util = require('./util');
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/* global global, Promise */
+import { stringify, exists, notExists, isObject,
+  resolveUrl, isNumber, templist, cloneObj } from './util';
 
 /** @const {function} ifEndForObjWalk - the function to find if no more iteration required */
-var ifEndForObjWalk = function ifEndForObjWalk(obj, depth) {
-  if (depth > _util.templist.config.maxobjdepth) return false;
-  if ((typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object' && obj !== null) {
+const ifEndForObjWalk = function ifEndForObjWalk(obj, depth) {
+  if (depth > templist.config.maxobjdepth) return false;
+  if (typeof obj === 'object' && obj !== null) {
     if (obj.from && obj._) {
-      var toRet = Object.keys(obj).filter(function (ky) {
-        return ky !== '_' && ky !== 'from';
-      });
+      const toRet = Object.keys(obj).filter(ky => (ky !== '_' && ky !== 'from'));
       toRet.toEndBulk = true;
       return toRet;
     }
@@ -35,30 +17,28 @@ var ifEndForObjWalk = function ifEndForObjWalk(obj, depth) {
 };
 
 /** @const {function} WALK - The function to iterate deeper in object */
-var WALK = function WALK(fun, rt, obj, key, depth) {
-  var dep = depth || 0;
+const WALK = function WALK(fun, rt, obj, key, depth) {
+  const dep = depth || 0;
   fun(obj, key, rt, dep || 0);
-  var ob = ifEndForObjWalk(obj, dep);
+  let ob = ifEndForObjWalk(obj, dep);
   if (ob) {
-    var kys = void 0;
+    let kys;
     if (Array.isArray(ob) && ob.toEndBulk === true) {
-      kys = ob.filter(function (ky) {
-        return ky !== 'toEndBulk';
-      });
+      kys = ob.filter(ky => ky !== 'toEndBulk');
       ob = obj;
     } else {
       kys = Object.keys(ob);
     }
-    var lastln = kys.length;
-    var deep = dep + 1;
-    for (var z = 0; z <= lastln; z += 1) {
+    const lastln = kys.length;
+    const deep = dep + 1;
+    for (let z = 0; z <= lastln; z += 1) {
       WALK(fun, ob, ob[kys[z]], kys[z], deep);
     }
   }
 };
 
 /** @const {function} CONVERT - the function to get replaced value */
-var CONVERT = _util.templist.create(0, Object.assign(_util.templist.utils, { objwalk: WALK }));
+const CONVERT = templist.create(0, Object.assign(templist.utils, { objwalk: WALK }));
 
 /**
  * @module bulkapi
@@ -73,26 +53,17 @@ var CONVERT = _util.templist.create(0, Object.assign(_util.templist.utils, { obj
  * A mock request class
  * @class
  */
-
-var MockReq = function MockReq() {
-  _classCallCheck(this, MockReq);
-};
+class MockReq {}
 /**
  * A mock response class
  * @class
  */
-
-
-var MockRes = function MockRes() {
-  _classCallCheck(this, MockRes);
-};
+class MockRes {}
 
 /**
  * A mock response methods
  */
-
-
-var MockResMethods = {
+const MockResMethods = {
   /**
    * create an instance of mock res
    * @param {function} resolve - call if operation is success
@@ -111,10 +82,10 @@ var MockResMethods = {
    * @param {Object} data - the response payload/object
    */
   over: function over(data) {
-    if (!this.finished) {
+    if (!(this.finished)) {
       this.finished = true;
-      var call = 'reject';
-      if (!(0, _util.isNumber)(this.statusCode) || this.statusCode > 199 && this.statusCode < 300) {
+      let call = 'reject';
+      if (!(isNumber(this.statusCode)) || (this.statusCode > 199 && this.statusCode < 300)) {
         call = 'resolve';
       }
       this[call](data === undefined ? this.responseText : data);
@@ -132,7 +103,7 @@ var MockResMethods = {
    * @param {string} data - the response payload/object
    */
   write: function write(data) {
-    this.responseText += (0, _util.stringify)(data);
+    this.responseText += stringify(data);
   },
   /**
    * set the status code
@@ -148,7 +119,7 @@ var MockResMethods = {
    * @param {Object} data - if num is not a number then it will be repsonse data
    */
   sending: function sending(num, data) {
-    if ((0, _util.isNumber)(num)) {
+    if (isNumber(num)) {
       this.status(num).over(data);
     } else {
       this.over(num);
@@ -169,18 +140,17 @@ var MockResMethods = {
    */
   send: function send(num, data) {
     this.sending(num, data);
-  }
+  },
 };
 
-var MockResKeys = Object.keys(MockResMethods);
+const MockResKeys = Object.keys(MockResMethods);
 
 /**
  * A class which has methods to convert single to number of apis
  *  and combine those back into single
  * @class
  */
-
-var BulkAPI = function () {
+class BulkAPI {
   /**
    * create an instance of BulkAPI
    * @param {function} handler - the function that will be called upon each of the api calls
@@ -191,21 +161,14 @@ var BulkAPI = function () {
    * @param {function} fail - the function that will be called
    *          when one of the API call fails
    */
-  function BulkAPI(handler) {
-    var mockReq = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : MockReq;
-    var mockRes = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : MockRes;
-    var succ = arguments[3];
-    var fail = arguments[4];
-
-    _classCallCheck(this, BulkAPI);
-
+  constructor(handler, mockReq = MockReq, mockRes = MockRes, succ, fail) {
     this.handler = handler;
-    this.success = typeof succ !== 'function' ? function success(data) {
-      this.end((0, _util.stringify)(data));
+    this.success = (typeof succ !== 'function') ? function success(data) {
+      this.end(stringify(data));
     } : succ;
-    this.failure = typeof fail !== 'function' ? function failure(err) {
+    this.failure = (typeof fail !== 'function') ? function failure(err) {
       this.statusCode = 409;
-      this.end((0, _util.stringify)(err.message || err));
+      this.end(stringify(err.message || err));
       console.log(err); // eslint-disable-line no-console
     } : fail;
     this.MockReq = mockReq;
@@ -219,207 +182,110 @@ var BulkAPI = function () {
    * @param {Object} payload - typically the request payload
    * @static
    */
-
-
-  _createClass(BulkAPI, [{
-    key: 'resolveRequest',
-
-
-    /*
-     * Resolve request object body, expand its body as another bulk api call
-     * @param {Object} reqObj - the current request api object
-     * @param {Object[]} resultArr - the current result array
-     */
-    value: function resolveRequest(reqObj, resultArr) {
-      var nowOb = { result: resultArr, rootResult: this.rootResult };
-      var from = CONVERT(reqObj.body && reqObj.body.from, nowOb);
-      if (Array.isArray(from)) {
-        Object.assign(reqObj.body, {
-          _: from.map(function (dt, ind) {
-            return CONVERT((0, _util.cloneObj)(reqObj.body._), Object.assign({
-              $data: dt,
-              $: ind
-            }, nowOb));
-          })
-        });
-      }
-      delete reqObj.body.from; // eslint-disable-line no-param-reassign
-      CONVERT(reqObj.body, nowOb);
-    }
-
-    /*
-     * resolve the paylaod
-     *  once payload is array (each must have body property),
-     * @param {Object} req - the request body for one api
-     * @return {Promise} promise - the resolution of all the apis
-     */
-
-  }, {
-    key: 'promisify',
-    value: function promisify(req) {
-      var _this = this;
-
-      return new Promise(function (res, rej) {
-        var nreq = new _this.MockReq(req);
-        Object.assign(nreq, req);
-        nreq.rootResult = _this.rootResult;
-        var nres = new _this.MockRes(nreq);
-        MockResKeys.forEach(function (ky) {
-          nres[ky] = MockResMethods[ky].bind(nres);
-        });
-        nres.initPromise(res, rej);
-        _this.handler(nreq, nres);
-      });
-    }
-
-    /*
-     * resolve the paylaod
-     * @param {Object} req - the incoming request instance
-     * @return {Object[]} res - the resolution of all the apis
-     */
-
-  }, {
-    key: 'resolve',
-    value: function () {
-      var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(req) {
-        var res, arr, ln, z, prmarr;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                res = [];
-
-                if (!Array.isArray(this.rootResult)) this.rootResult = res;
-                this.resolveRequest(req, res);
-                arr = BulkAPI.divide(req.body);
-                ln = arr.length;
-                z = 0;
-
-              case 6:
-                if (!(z < ln)) {
-                  _context.next = 16;
-                  break;
-                }
-
-                if (!arr[z].method) {
-                  _context.next = 11;
-                  break;
-                }
-
-                return _context.abrupt('break', 16);
-
-              case 11:
-                this.resolveRequest(arr[z], res);
-                res.push(arr[z].body);
-
-              case 13:
-                z += 1;
-                _context.next = 6;
-                break;
-
-              case 16:
-                if (!(z < ln)) {
-                  _context.next = 30;
-                  break;
-                }
-
-                if (!arr[z].first) {
-                  _context.next = 26;
-                  break;
-                }
-
-                this.resolveRequest(arr[z], res);
-                _context.t0 = res;
-                _context.next = 22;
-                return this.promisify(arr[z]);
-
-              case 22:
-                _context.t1 = _context.sent;
-
-                _context.t0.push.call(_context.t0, _context.t1);
-
-                _context.next = 27;
-                break;
-
-              case 26:
-                return _context.abrupt('break', 30);
-
-              case 27:
-                z += 1;
-                _context.next = 16;
-                break;
-
-              case 30:
-                prmarr = [];
-
-                for (; z < ln; z += 1) {
-                  this.resolveRequest(arr[z], res);
-                  prmarr.push(this.promisify(arr[z]));
-                }
-                _context.t2 = res.push;
-                _context.t3 = res;
-                _context.t4 = _toConsumableArray;
-                _context.next = 37;
-                return Promise.all(prmarr);
-
-              case 37:
-                _context.t5 = _context.sent;
-                _context.t6 = (0, _context.t4)(_context.t5);
-
-                _context.t2.apply.call(_context.t2, _context.t3, _context.t6);
-
-                return _context.abrupt('return', res);
-
-              case 41:
-              case 'end':
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
-
-      function resolve(_x3) {
-        return _ref.apply(this, arguments);
-      }
-
-      return resolve;
-    }()
-
-    /*
-     * call bulk for express like handlers
-     */
-
-  }, {
-    key: 'callbulk',
-    value: function callbulk(req, res) {
-      this.rootResult = req.rootResult;
-      this.resolve(req).then(this.success.bind(res)).catch(this.failure.bind(res));
-    }
-  }], [{
-    key: 'divide',
-    value: function divide(payload) {
-      if ((typeof payload === 'undefined' ? 'undefined' : _typeof(payload)) !== 'object' || payload === null) return [];
-      var base = payload.base;
-      if (!(0, _util.isObject)(base)) base = {};
-      return (Array.isArray(payload._) ? payload._ : payload._ ? [payload._] : Array.isArray(payload) ? payload : [payload]).map(function (ob) {
-        return {
-          method: ob.method || base.method,
-          url: (0, _util.resolveUrl)((0, _util.stringify)((0, _util.exists)(base.url) ? base.url : ''), (0, _util.stringify)((0, _util.exists)(ob.url) ? ob.url : '')),
-          headers: Object.assign({}, base.headers, ob.headers),
-          body: Object.assign(Array.isArray(ob.body) ? new Array(ob.body.length) : {}, base.body, ob.body),
-          first: (0, _util.exists)(ob.first) ? Boolean(ob.first) : Boolean(base.first)
-        };
-      }).sort(function (oa, ob) {
-        var sc = function sc(ab) {
-          return (0, _util.notExists)(ab.method) ? 0 : ab.first ? 1 : 2;
-        };
+  static divide(payload) {
+    if (typeof payload !== 'object' || payload === null) return [];
+    let base = payload.base;
+    if (!(isObject(base))) base = {};
+    return (Array.isArray(payload._) ? payload._ : (payload._ ? [payload._] :
+      (Array.isArray(payload) ? payload : [payload]))).map(ob => ({
+        method: ob.method || base.method,
+        url: resolveUrl(stringify(exists(base.url) ? base.url : ''),
+                stringify(exists(ob.url) ? ob.url : '')),
+        headers: Object.assign({}, base.headers, ob.headers),
+        body: Object.assign(Array.isArray(ob.body) ? new Array(ob.body.length) : {},
+          base.body, ob.body),
+        first: exists(ob.first) ? Boolean(ob.first) : Boolean(base.first),
+      })).sort((oa, ob) => {
+        const sc = ab => (notExists(ab.method) ? 0 : (ab.first ? 1 : 2));
         return sc(oa) - sc(ob);
       });
+  }
+
+  /*
+   * Resolve request object body, expand its body as another bulk api call
+   * @param {Object} reqObj - the current request api object
+   * @param {Object[]} resultArr - the current result array
+   */
+  resolveRequest(reqObj, resultArr) {
+    const nowOb = { result: resultArr, rootResult: this.rootResult };
+    const from = CONVERT(reqObj.body && reqObj.body.from, nowOb);
+    if (Array.isArray(from)) {
+      Object.assign(reqObj.body, {
+        _: from.map((dt, ind) => CONVERT(cloneObj(reqObj.body._), Object.assign({
+          $data: dt,
+          $: ind,
+        }, nowOb))),
+      });
     }
-  }]);
+    delete reqObj.body.from; // eslint-disable-line no-param-reassign
+    CONVERT(reqObj.body, nowOb);
+  }
 
-  return BulkAPI;
-}();
+  /*
+   * resolve the paylaod
+   *  once payload is array (each must have body property),
+   * @param {Object} req - the request body for one api
+   * @return {Promise} promise - the resolution of all the apis
+   */
+  promisify(req) {
+    return new Promise((res, rej) => {
+      const nreq = new (this.MockReq)(req);
+      Object.assign(nreq, req);
+      nreq.rootResult = this.rootResult;
+      const nres = new (this.MockRes)(nreq);
+      MockResKeys.forEach((ky) => {
+        nres[ky] = MockResMethods[ky].bind(nres);
+      });
+      nres.initPromise(res, rej);
+      this.handler(nreq, nres);
+    });
+  }
 
-exports.default = BulkAPI;
-exports.MockResMethods = MockResMethods;
+  /*
+   * resolve the paylaod
+   * @param {Object} req - the incoming request instance
+   * @return {Object[]} res - the resolution of all the apis
+   */
+  async resolve(req) {
+    const res = [];
+    if (!Array.isArray(this.rootResult)) this.rootResult = res;
+    this.resolveRequest(req, res);
+    const arr = BulkAPI.divide(req.body);
+    const ln = arr.length;
+    let z = 0;
+    for (; z < ln; z += 1) {
+      if (arr[z].method) {
+        break;
+      } else {
+        this.resolveRequest(arr[z], res);
+        res.push(arr[z].body);
+      }
+    }
+    for (; z < ln; z += 1) {
+      if (arr[z].first) {
+        this.resolveRequest(arr[z], res);
+        res.push(await this.promisify(arr[z])); // eslint-disable-line no-await-in-loop
+      } else {
+        break;
+      }
+    }
+    const prmarr = [];
+    for (; z < ln; z += 1) {
+      this.resolveRequest(arr[z], res);
+      prmarr.push(this.promisify(arr[z]));
+    }
+    res.push(...(await Promise.all(prmarr)));
+    return res;
+  }
+
+  /*
+   * call bulk for express like handlers
+   */
+  callbulk(req, res) {
+    this.rootResult = req.rootResult;
+    this.resolve(req).then(this.success.bind(res)).catch(this.failure.bind(res));
+  }
+
+}
+
+export { BulkAPI as default, MockResMethods };
--- util.js
+++ util.js
@@ -1,43 +1,22 @@
-'use strict';
+import templist from 'templist';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.isAlphaNum = exports.cloneObj = exports.parseObj = exports.resolveUrl = exports.templist = exports.assign = exports.objwalk = exports.getString = exports.stringify = exports.exists = exports.notExists = exports.isNumber = exports.isString = exports.isObject = undefined;
+const isObject = ob => (typeof ob === 'object' && ob !== null && !(Array.isArray(ob)));
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+const isString = ob => (typeof ob === 'string' && ob.length > 0);
 
-var _templist = require('templist');
+const isNumber = ob => (typeof ob === 'number' && ob !== isNaN && ob > 0);
 
-var _templist2 = _interopRequireDefault(_templist);
+const notExists = ob => (typeof ob === 'undefined' || ob === null);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const exists = ob => !(notExists(ob));
 
-var isObject = function isObject(ob) {
-  return (typeof ob === 'undefined' ? 'undefined' : _typeof(ob)) === 'object' && ob !== null && !Array.isArray(ob);
-};
-
-var isString = function isString(ob) {
-  return typeof ob === 'string' && ob.length > 0;
-};
-
-var isNumber = function isNumber(ob) {
-  return typeof ob === 'number' && ob !== isNaN && ob > 0;
-};
-
-var notExists = function notExists(ob) {
-  return typeof ob === 'undefined' || ob === null;
-};
-
-var exists = function exists(ob) {
-  return !notExists(ob);
-};
-
-var stringify = function stringify(input, pretty) {
+const stringify = (input, pretty) => {
   if (typeof input === 'string') return input;
-  if ((typeof input === 'undefined' ? 'undefined' : _typeof(input)) === 'object') {
+  if (typeof input === 'object') {
     try {
-      return pretty ? JSON.stringify(input, undefined, isString(pretty) ? pretty : '  ') : JSON.stringify(input);
+      return pretty
+        ? JSON.stringify(input, undefined, isString(pretty) ? pretty : '  ')
+        : JSON.stringify(input);
     } catch (err) {
       return String(input);
     }
@@ -45,23 +24,19 @@ var stringify = function stringify(input, pretty) {
   return String(input);
 };
 
-var getString = function getString(input) {
-  for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    args[_key - 1] = arguments[_key];
-  }
-
-  return typeof input === 'function' ? input(args) : stringify(input);
+const getString = function getString(input, ...args) {
+  return (typeof input === 'function') ? input(args) : stringify(input);
 };
 
-var resolveUrl = function resolveUrl(urlA, urlB) {
-  var ua = stringify(urlA);
-  var ub = stringify(urlB);
+const resolveUrl = function resolveUrl(urlA, urlB) {
+  let ua = stringify(urlA);
+  let ub = stringify(urlB);
   if (ua.endsWith('/')) ua = ua.slice(0, -1);
-  if (!ub.startsWith('/') && !ub.startsWith('http')) ub = '/' + ub;
+  if (!(ub.startsWith('/')) && !(ub.startsWith('http'))) ub = `/${ub}`;
   return ua + ub;
 };
 
-var parseObj = function parseObj(obj) {
+const parseObj = function parseObj(obj) {
   if (typeof obj === 'string') {
     try {
       return JSON.parse(obj);
@@ -72,25 +47,16 @@ var parseObj = function parseObj(obj) {
   return obj;
 };
 
-var cloneObj = function cloneObj(obj) {
+const cloneObj = function cloneObj(obj) {
   return parseObj(stringify(obj));
 };
 
-var assign = _templist2.default.utils.assign;
-var objwalk = _templist2.default.utils.objwalk;
-var isAlphaNum = _templist2.default.utils.isAlphaNum;
+const assign = templist.utils.assign;
+const objwalk = templist.utils.objwalk;
+const isAlphaNum = templist.utils.isAlphaNum;
 
-exports.isObject = isObject;
-exports.isString = isString;
-exports.isNumber = isNumber;
-exports.notExists = notExists;
-exports.exists = exists;
-exports.stringify = stringify;
-exports.getString = getString;
-exports.objwalk = objwalk;
-exports.assign = assign;
-exports.templist = _templist2.default;
-exports.resolveUrl = resolveUrl;
-exports.parseObj = parseObj;
-exports.cloneObj = cloneObj;
-exports.isAlphaNum = isAlphaNum;
+export {
+  isObject, isString, isNumber, notExists, exists,
+  stringify, getString, objwalk, assign, templist,
+  resolveUrl, parseObj, cloneObj, isAlphaNum,
+};
